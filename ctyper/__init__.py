468b45dca5994162b0e131a5abe7e07528592e62
Sped0n <realsped0n@outlook.com>
2023-07-20 16:39:06 +0800

    feat(ctyper): add custom dataclasses and type alias
    This commit adds custom dataclasses `InputSize` and `MLPreprocessParams` to the `ctyper` package. It also introduces type aliases for `Image`, `Color`, and `Array`. These additions enhance the functionality of the package by providing structured representations for input sizes and machine learning preprocess parameters.

--- ctyper/__init__.py
+++ ctyper/__init__.py
@@ -1,10 +1,30 @@
-import numpy as np
+import dataclasses
 from typing import TypeAlias
+
+import numpy as np
 
 # custom type alias
 Image: TypeAlias = np.ndarray
 Color: TypeAlias = tuple[int, int, int]
 Array: TypeAlias = np.ndarray
+
+
+# custom dataclass
+@dataclasses.dataclass
+class InputSize:
+    w: int
+    h: int
+
+
+@dataclasses.dataclass
+class MLPreprocessParams:
+    w0: int
+    h0: int
+    w1: int
+    h1: int
+    wpad: int
+    hpad: int
+    scale: float
 
 
 # custom exceptions
