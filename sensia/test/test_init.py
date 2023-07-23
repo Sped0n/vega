74a0af2df12171bcd9ec04a4efccf2b67ed04f2c
Sped0n <realsped0n@outlook.com>
2023-07-24 05:54:33 +0800

    test(sensia): add tests for T265 and D435 initialization and fetching
    This commit adds new test functions to test the initialization and fetching of T265 and D435 devices. The tests check if the devices are properly initialized, if data can be fetched from them, and if the fetched data has the expected properties. Additionally, error handling is implemented to handle cases where no device is available or there are issues with initializing or fetching data from the devices.
    
    The added test functions include:
    - `test_t265_init()`: Tests the initialization of T265 device.
    - `test_t265_fetch()`: Tests fetching data from T265 device.
    - `test_d435_init()`: Tests the initialization of D435 device.
    - `test_d435_fetch_depth_only()`: Tests fetching depth-only data from D435 device.
    - `test_d435_fetch_default()`: Tests fetching default (depth + color) data from D435 device.
    - `test_d435_fetch_default_align()`: Tests fetching default (depth + color) aligned data from D435 device.
    - `test_d435_fetch_hd()`: Tests fetching HD (depth + color) data from D435 device.
    - `test_d435_fetch_hd_align()`: Tests fetching HD (depth + color) aligned data from D435 device.
    
    These tests ensure that the T265 and D345 devices are functioning correctly and provide a baseline for future development.

--- sensia/test/test_init.py
+++ sensia/test/test_init.py
@@ -1,11 +1,14 @@
 import pyrealsense2 as rs
+import os
+from ctyper import DeviceInitError, FetchError
 
-from sensia import T265
+from sensia import T265, D435
+from warnings import warn
 
 ctx = rs.context()
 no_device = False
 if len(ctx.query_devices()) == 0:
-    print("\nno device, skipping test")
+    warn("no device, skipping test")
     no_device = True
 
 
@@ -13,12 +16,17 @@ def test_t265_init():
     if not no_device:
         t = T265()
         assert isinstance(t, T265)
+        t.stop()
 
 
 def test_t265_fetch():
     if not no_device:
         t = T265()
-        d = t.fetch()
+        try:
+            d = t.fetch()
+        except FetchError:
+            t.restart()
+            d = t.fetch()
         assert isinstance(d.roll, float)
         assert isinstance(d.pitch, float)
         assert isinstance(d.yaw, float)
@@ -26,3 +34,109 @@ def test_t265_fetch():
         assert isinstance(d.y, float)
         assert isinstance(d.z, float)
         assert isinstance(d.confidence, int)
+        t.stop()
+
+
+def test_d435_init():
+    if not no_device:
+        try:
+            h = D435()
+            assert isinstance(h, D435)
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
+
+
+def test_d435_fetch_depth_only():
+    if not no_device:
+        try:
+            h = D435(depth_only=True)
+            try:
+                d = h.fetch()
+            except FetchError:
+                h.restart()
+                d = h.fetch()
+            assert d.dvalid is True
+            assert d.depth.shape == (720, 1280)
+            assert d.cvalid is False
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
+
+
+def test_d435_fetch_default():
+    if not no_device:
+        try:
+            h = D435()
+            try:
+                d = h.fetch()
+            except FetchError:
+                h.restart()
+                d = h.fetch()
+            assert d.dvalid is True
+            assert d.depth.shape == (720, 1280)
+            assert d.cvalid is True
+            assert d.color.shape == (720, 1280, 3)
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
+
+
+def test_d435_fetch_default_align():
+    if not no_device:
+        try:
+            h = D435(align=True)
+            try:
+                d = h.fetch()
+            except FetchError:
+                h.restart()
+                d = h.fetch()
+            assert d.dvalid is True
+            assert d.depth.shape == (720, 1280)
+            assert d.cvalid is True
+            assert d.color.shape == (720, 1280, 3)
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
+
+
+def test_d435_fetch_hd():
+    if not no_device:
+        try:
+            h = D435(hd=True)
+            try:
+                d = h.fetch()
+            except FetchError:
+                h.restart()
+                d = h.fetch()
+            assert d.dvalid is True
+            assert d.depth.shape == (720, 1280)
+            assert d.cvalid is True
+            assert d.color.shape == (1080, 1920, 3)
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
+
+
+def test_d435_fetch_hd_align():
+    if not no_device:
+        try:
+            h = D435(hd=True, align=True)
+            try:
+                d = h.fetch()
+            except FetchError:
+                h.restart()
+                d = h.fetch()
+            assert d.dvalid is True
+            assert d.depth.shape == (1080, 1920)
+            assert d.cvalid is True
+            assert d.color.shape == (1080, 1920, 3)
+            h.stop()
+        except RuntimeError or DeviceInitError:
+            assert os.system == "Darwin"
+            warn("pyrs2 issue on macos")
