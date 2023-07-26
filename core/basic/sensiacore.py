4dd89facf8c6cf9d2b9f048fb2838ef2a544ee8e
Sped0n <realsped0n@outlook.com>
2023-07-27 04:01:48 +0800

    refactor(core): simplify core and tx functions
    - Removed unnecessary loop_condition method in both sensiacore.py and vgcore.py.
    - Simplified the core function by removing the if-else statement for checking queue status. Now, it directly puts data into the pose_queue without any condition.
    - Simplified the tx function by removing the if-else statement for checking queue status. Now, it directly puts data into the tx_queue without any condition.
    - Added a call to flush_queue function when pose_queue or tx_queue is full to clear out old data.
    
    This commit simplifies the code logic in sensiacore.py and vgcore.py, making it more concise and efficient.

--- core/basic/sensiacore.py
+++ core/basic/sensiacore.py
@@ -3,34 +3,26 @@ from time import sleep
 from sensia import T265
 from multiprocessing import Queue as mQueue
 from itertools import count
+from core.utils import flush_queue
 
 
 class basic:
-    def __init__(self, pose_queue: mQueue, debug: bool = False) -> None:
+    def __init__(self, pose_queue: mQueue) -> None:
         self.t = T265()
         self.pose_queue = pose_queue
-        self.debug = debug
-        if self.debug:
-            self.loop_limit = count()
         self.run()
 
-    def loop_condition(self):
-        if self.debug:
-            return next(self.loop_limit) <= 500000
-        return True
-
     def core(self):
-        while self.loop_condition():
-            if not self.pose_queue.full():
-                self.pose_queue.put(self.t.fetch())
-            else:
-                pass
+        while True:
+            if self.pose_queue.full():
+                flush_queue(self.pose_queue)
+            self.pose_queue.put(self.t.fetch())
 
     def run(self):
         # warm up
-        sleep(1)
+        sleep(0.5)
 
-        core_thread = Thread(target=self.core)
+        core_thread = Thread(target=self.core, daemon=True)
         core_thread.start()
         core_thread.join()
         self.t.stop()
--- core/basic/vgcore.py
+++ core/basic/vgcore.py
@@ -1,10 +1,10 @@
-from itertools import count
 from multiprocessing import Queue as mQueue
-from queue import Empty, Queue
+from queue import Queue
 from random import random
 from threading import Thread
 from time import sleep
 
+from core.utils import flush_queue
 from sensia.utils import PoseData
 
 
@@ -36,52 +36,36 @@ class toy_prototype:
 
 
 class basic:
-    def __init__(self, pose_queue: mQueue, debug: bool = False) -> None:
+    def __init__(self, pose_queue: mQueue) -> None:
         self.pose_queue = pose_queue
         self.tx_queue = Queue(5)
-        self.debug = debug
-        if self.debug:
-            self.loop_limit = count()
         self.run()
 
-    def loop_condition(self):
-        if self.debug:
-            return next(self.loop_limit) <= 500000
-        return True
-
     def tx(self):
-        while self.loop_condition():
-            if not self.tx_queue.empty():
-                try:
-                    tmp = self.tx_queue.get(timeout=0.5)
-                    assert len(tmp) == 3
-                    assert isinstance(tmp[0], int)
-                    assert isinstance(tmp[1], int)
-                    assert isinstance(tmp[2], int)
-                except Empty:
-                    continue
-            else:
-                pass
+        while True:
+            tmp = self.tx_queue.get()
+            assert len(tmp) == 3
+            assert isinstance(tmp[0], int)
+            assert isinstance(tmp[1], int)
+            assert isinstance(tmp[2], int)
 
     def core(self):
-        while self.loop_condition():
-            if not self.pose_queue.empty() and not self.tx_queue.full():
-                try:
-                    data: PoseData = self.pose_queue.get(timeout=0.5)
-                except Empty:
-                    continue
-                tmp = [int(data.roll), int(data.pitch), int(data.yaw)]
-                self.tx_queue.put(tmp)
-            else:
-                pass
+        while True:
+            data: PoseData = self.pose_queue.get()
+            tmp = [int(data.roll), int(data.pitch), int(data.yaw)]
+            if self.tx_queue.full():
+                flush_queue(self.tx_queue)
+            self.tx_queue.put(tmp)
 
     def run(self):
         # warm up
-        sleep(1)
+        sleep(0.5)
 
-        core_thread = Thread(target=self.core)
+        core_thread = Thread(target=self.core, daemon=True)
+        tx_thread = Thread(target=self.tx, daemon=True)
+
         core_thread.start()
-        tx_thread = Thread(target=self.tx)
         tx_thread.start()
+
         core_thread.join()
         tx_thread.join()
