0e9db638d987bfe5cd2f1be8250eff9652ef747b
Sped0n <realsped0n@outlook.com>
2023-07-29 05:54:42 +0800

    refactor(cfg): improve color generation in __init__.py
    Refactored the color generation logic in the `cfg/__init__.py` file to use the `Color` class from `ctyper`. This change improves the readability and maintainability of the code by explicitly defining the type of `colors_80` as a list of `Color` objects. The previous implementation used a nested list comprehension to generate random RGB values for each color, which could lead to confusion and potential errors.

--- cfg/__init__.py
+++ cfg/__init__.py
@@ -2,6 +2,8 @@ import os
 import platform
 import random
 
+from ctyper import Color
+
 is_x86: bool = platform.machine() in ("i386", "AMD64", "x86_64")
 is_arm: bool = platform.machine() in ("arm64", "aarch64")
 is_darwin: bool = platform.system() == "Darwin"
@@ -9,4 +11,6 @@ is_linux: bool = platform.system() == "Linux"
 
 VDBG: bool = str(os.environ.get("VDBG")) == "1"
 
-colors_80 = [[random.randint(0, 255) for _ in range(3)] for _ in range(80)]
+colors_80: list[Color] = [
+    tuple([random.randint(0, 255) for _ in range(3)]) for _ in range(80)
+]
