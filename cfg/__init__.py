830902d0470e25674527c928255c75516ebd9ee7
Sped0n <realsped0n@outlook.com>
2023-07-29 05:40:50 +0800

    feat(cfg): add random color generation
    This commit adds the ability to generate random colors in the cfg module. A new list, `colors_80`, is created with 80 sublists, each containing 3 randomly generated integers between 0 and 255. This feature enhances the functionality of the cfg module by providing a convenient way to generate a variety of colors.

--- cfg/__init__.py
+++ cfg/__init__.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import random
 
 is_x86: bool = platform.machine() in ("i386", "AMD64", "x86_64")
 is_arm: bool = platform.machine() in ("arm64", "aarch64")
@@ -7,3 +8,5 @@ is_darwin: bool = platform.system() == "Darwin"
 is_linux: bool = platform.system() == "Linux"
 
 VDBG: bool = str(os.environ.get("VDBG")) == "1"
+
+colors_80 = [[random.randint(0, 255) for _ in range(3)] for _ in range(80)]
