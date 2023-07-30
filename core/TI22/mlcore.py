ec7a4ce5dbcbb207b2f6e148d3f327bbe08abba5
Sped0n <realsped0n@outlook.com>
2023-07-31 01:48:28 +0800

    feat(TI22): add mlcore module for TI22
    This commit adds the `mlcore` module for the TI22 project. The `mlcore.py` file contains the implementation of the `proc` class, which handles the processing of input data from camera and communication with other modules through queues. It also initializes the TI2022 model and includes methods for inference and command management.
    
    The `proc` class has three input/output queues: `cam_queue`, `vega2ml_queue`, and `ml2vega_queue`. It uses these queues to receive image frames from the camera, receive commands from other modules, and send processed results back to other modules respectively.
    
    The main functionalities of this module are:
    - Initializing the TI2022 model with a specific directory path
    - Processing incoming frames using the TI2022 model's inference method
    - Pushing processed results to the `ml2vega_queue`
    - Managing commands received from other modules through the `vega2ml_queue`
    
    Additionally, this commit includes two threads: one for managing commands (`manager_thread`) and another for running inference (`ti2022_thread`). These threads are started in the constructor of the `proc` class and joined at the end.
    
    Overall, this commit introduces a new module that plays a crucial role in handling machine learning operations for TI22 project.

---
+++ core/TI22/mlcore.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from multiprocessing import Queue as mQueue
+from pathlib import Path
+from threading import Event, Thread
+from time import sleep
+
+from core.utils import get_cmd, pusher
+from ctyper import Command, Image, InputSize, ObjDetected
+from ml import Model
+
+DATA_DIR = str(Path(__file__).resolve().parent / "data") + "/"
+
+
+class proc:
+    def __init__(
+        self,
+        cam_queue: mQueue[Image],  # input
+        vega2ml_queue: mQueue[Command],  # input
+        ml2vega_queue: mQueue[dict],  # output
+    ) -> None:
+        isize = InputSize(416, 416)
+        # model init
+        self.ti = Model(DATA_DIR + "ti2022", isize, "ncnn")
+
+        # proc queue init
+        self.cam_queue = cam_queue
+        self.ml2vega_queue = ml2vega_queue
+        self.vega2ml_queue = vega2ml_queue
+
+        # default enable options
+        self.ti_enable: Event = Event()
+        self.ti_enable.clear()  # default disable
+
+        self.run()
+
+    def ti2022(self):
+        while True:
+            self.ti_enable.wait()
+
+            # data fetch
+            frame = self.cam_queue.get()
+
+            # data process
+            results = self.ti.infer(frame)
+            pusher(self.ml2vega_queue, {"ti": results})
+
+    def manager(self):
+        while True:
+            # no new command, continue
+            cmd: Command = self.vega2ml_queue.get()
+            get_cmd(cmd, "ti", self.ti_enable)
+
+    def run(self):
+        # warm up
+        sleep(1)
+
+        manager_thread = Thread(target=self.manager, daemon=True)
+        ti2022_thread = Thread(target=self.ti2022, daemon=True)
+
+        manager_thread.start()
+        ti2022_thread.start()
+
+        manager_thread.join()
+        ti2022_thread.join()
