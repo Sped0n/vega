27700a8934fde4dbc3649107756794568bd890c3
Sped0n <realsped0n@outlook.com>
2023-07-31 01:45:33 +0800

    refactor(cfg): refactor initialization and configuration
    Refactored the initialization and configuration code in `cfg/__init__.py` to improve readability and maintainability. Introduced a try-except block to handle different import scenarios for the `pyrealsense2` module. Added visual debug mode (`VDBG`) and no realsense mode (`NORS`) flags for better control over debugging and testing. Also added a warning message if no realsense device is connected, forcing the `NORS` flag to be set.

--- cfg/__init__.py
+++ cfg/__init__.py
@@ -1,6 +1,16 @@
+try:
+    import pyrealsense2 as rs
+
+    assert "2." in str(rs.__version__)
+except AttributeError:
+    import pyrealsense2.pyrealsense2 as rs
+
+    assert "2." in str(rs.__version__)
+
 import os
 import platform
 import random
+from warnings import warn
 
 from ctyper import Color
 
@@ -9,7 +19,15 @@ is_arm: bool = platform.machine() in ("arm64", "aarch64")
 is_darwin: bool = platform.system() == "Darwin"
 is_linux: bool = platform.system() == "Linux"
 
+# visual debug mode
 VDBG: bool = str(os.environ.get("VDBG")) == "1"
+
+# no realsense mode
+NORS: bool = str(os.environ.get("NORS")) == "1"
+if len(rs.context().query_devices()) == 0:
+    warn("no realsense device, skipping test")
+    NORS = True  # force NORS if no device connected
+
 
 colors_80: list[Color] = [
     tuple([random.randint(0, 255) for _ in range(3)]) for _ in range(80)
