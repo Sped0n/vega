c56ddf4fe456d74332725a56fb8efac64bd03084
Sped0n <realsped0n@outlook.com>
2023-07-31 01:54:11 +0800

    test(TI22): refactor test_ti22 function
    Refactored the `test_ti22` function in the `core/test/TI22/test_ti22.py` file.
    - Added a condition to check if NORS is False before initializing additional queues and processes.
    - Added a new queue and process for mlcore.
    - Updated the arguments passed to existing processes to include the new queues.
    - Added a new process for mlproc.
    - Introduced a sleep of 10 seconds after starting all processes.
    - Killed all processes after the sleep period.

--- core/test/TI22/test_ti22.py
+++ core/test/TI22/test_ti22.py
@@ -1,59 +1,77 @@
 from multiprocessing import Process
 from multiprocessing import Queue as mQueue
-from time import time
+from time import sleep
 
+from cfg import NORS
 from core.TI22.sensiacore import proc as sensiaproc
 from core.TI22.vgcore import proc as vgproc
 from core.TI22.visioncore import proc as visionproc
+from core.TI22.mlcore import proc as mlproc
 
 
 def test_ti22():
-    pose_q = mQueue(5)
-    depth_q = mQueue(5)
-    vega2sensia_q = mQueue(5)
-    vega2vision_q = mQueue(5)
-    hula2vega_q = mQueue(5)
+    if NORS is False:
+        pose_q = mQueue(5)
+        depth_q = mQueue(5)
+        cam_q = mQueue(3)
+        vega2sensia_q = mQueue(5)
+        vega2vision_q = mQueue(5)
+        vega2ml_q = mQueue(5)
+        hula2vega_q = mQueue(5)
+        ti2vega_q = mQueue(5)
 
-    cp = Process(
-        target=vgproc,
-        args=(
-            pose_q,
-            hula2vega_q,
-            vega2vision_q,
-            vega2sensia_q,
-        ),
-        daemon=True,
-    )
+        cp = Process(
+            target=vgproc,
+            args=(
+                pose_q,
+                hula2vega_q,
+                ti2vega_q,
+                vega2vision_q,
+                vega2sensia_q,
+                vega2ml_q,
+            ),
+            daemon=True,
+        )
 
-    sp = Process(
-        target=sensiaproc,
-        args=(
-            pose_q,
-            depth_q,
-            vega2sensia_q,
-        ),
-        daemon=True,
-    )
+        sp = Process(
+            target=sensiaproc,
+            args=(
+                pose_q,
+                depth_q,
+                cam_q,
+                vega2sensia_q,
+            ),
+            daemon=True,
+        )
 
-    vp = Process(
-        target=visionproc,
-        args=(
-            depth_q,
-            vega2vision_q,
-            hula2vega_q,
-        ),
-        daemon=True,
-    )
+        vp = Process(
+            target=visionproc,
+            args=(
+                depth_q,
+                vega2vision_q,
+                hula2vega_q,
+            ),
+            daemon=True,
+        )
 
-    cp.start()
-    sp.start()
-    vp.start()
+        mp = Process(
+            target=mlproc,
+            args=(
+                cam_q,
+                vega2ml_q,
+                ti2vega_q,
+            ),
+            daemon=True,
+        )
 
-    start = time()
+        cp.start()
+        sp.start()
+        vp.start()
+        mp.start()
 
-    while time() - start < 10:
-        pass
+        sleep(10)
 
-    cp.kill()
-    sp.kill()
-    vp.kill()
+        cp.kill()
+        sp.kill()
+        vp.kill()
+        mp.kill()
