fe2b746ce527032200cd9316380d85fe44bd89d1
Sped0n <realsped0n@outlook.com>
2023-08-02 02:14:34 +0800

    feat(TI22): Add pusher and depack_recv_list_to_z functions
    This commit adds the `pusher` function to the `vgcore/__init__.py` file, which is used to push data into a queue. It also adds the `depack_recv_list_to_z` function from the `pin.utils` module, which is used to unpack received data and extract a specific value. These changes improve the functionality of the code by enhancing data handling and processing.

--- core/TI22/vgcore/__init__.py
+++ core/TI22/vgcore/__init__.py
@@ -7,9 +7,12 @@ from time import sleep, time
 
 from objprint import op
 
-from core.utils import DroneInfo, flush_queue, set_cmd
+from core.utils import DroneInfo, flush_queue, set_cmd, pusher
+from ctyper import PackCorruptedError
 from sensia.utils import PoseData
-from pin import create_uart_buf
+from pin.utils import create_uart_buf, depack_recv_list_to_z
+from pin import SerDevice
+from cfg import is_darwin, is_linux, is_arm, SER
 
 
 class proc:
@@ -33,25 +36,49 @@ class proc:
         # thread queue init
         self.tx_queue = Queue(5)
         self.status_queue: Queue[DroneInfo] = Queue(3)
-        self.target_array: Queue[DroneInfo] = Queue(3)
+        self.target_queue: Queue[DroneInfo] = Queue(3)
+        self.z_queue: Queue[int] = Queue(3)
+
+        # serial device
+        if is_darwin and SER is True:
+            self.uart = SerDevice(port="usbserial", baudrate=115200)
+        elif is_linux and is_arm and SER is True:
+            self.uart = SerDevice(port="/dev/ttyS3", baudrate=115200)
 
         # run it
         self.run()
 
     def tx(self):
-        curr_target = DroneInfo(0, 0, 0, 0)
+        curr_target = DroneInfo(0, 0, 0, 0, False)
+        txstart = time()
         while True:
             curr_pos = self.tx_queue.get()
-            # refresh target
-            if self.target_array.empty is False:
-                curr_target = self.target_array.get()
-            # create uart buf
-            tmp = create_uart_buf(current=curr_pos, target=curr_target, land=False)
+            # refresh target if not empty
+            if self.target_queue.empty is False:
+                curr_target = self.target_queue.get()
 
-            # simulate tx
-            # print(tmp)
-            # check the data type
-            print(tmp)
+            if SER is True:
+                # create uart buf
+                tmp = create_uart_buf(current=curr_pos, target=curr_target)
+                self.uart.write(tmp)
+            else:
+                if time() - txstart > 1:
+                    txstart = time()
+                    op(curr_pos, curr_target)
+
+    def rx(self):
+        while True:
+            # read from uart
+            if SER is False:
+                continue
+            tmp = self.uart.read_buf_to_list()
+            try:
+                z: int = depack_recv_list_to_z(tmp)
+            except PackCorruptedError:
+                continue
+            # push to queue
+            print(z)
+            pusher(self.z_queue, z)
 
     def pose_handler(self):
         while True:
@@ -76,9 +103,9 @@ class proc:
                 case 0:
                     set_cmd(self.vega2ml_queue, "ti", True)
                     set_cmd(self.vega2sensia_queue, "cam", True)
-                    start = time()
+                    mlstart = time()
                     self.ml2vega_queue.get()["ti"]
-                    print("ml process fps: ", 1 / (time() - start))
+                    print("ml process fps: ", 1 / (time() - mlstart))
                     count += 1
                     if count >= 60:
                         case = 1
@@ -97,13 +124,16 @@ class proc:
         sleep(1)
 
         tx_thread = Thread(target=self.tx, daemon=True)
+        rx_thread = Thread(target=self.rx, daemon=True)
         pose_thread = Thread(target=self.pose_handler, daemon=True)
         mission_thread = Thread(target=self.missionary, daemon=True)
 
         tx_thread.start()
+        rx_thread.start()
         pose_thread.start()
         mission_thread.start()
 
         tx_thread.join()
+        rx_thread.join()
         pose_thread.join()
         mission_thread.join()
