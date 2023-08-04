1d72b5c947520e1140a0f0682ad2a53f4fdc9417
Sped0n <realsped0n@outlook.com>
2023-08-05 06:02:38 +0800

    test(lumanos): add display test

--- lumanos/test/test_init.py
+++ lumanos/test/test_init.py
@@ -5,7 +5,15 @@ import cv2
 
 from cfg import VDBG
 from lumanos.device import Mocker
-from lumanos.TI23 import mapper, trailer
+from lumanos.TI23 import (
+    mapper,
+    trailer,
+    welcome_screen,
+    basic_confirm,
+    advanced_confirm,
+    basic_layout,
+    adcanced_layout,
+)
 from luma.core.render import canvas
 from compass import corrd2block
 from lumanos.utils import Offsetter
@@ -25,6 +33,111 @@ def test_ti23_mapper():
             mapper(draw)
             t.darw(draw)
 
+        sleep(0.02)
+        if time() - start > 3:
+            break
+    device.clear()
+
+    assert dq.empty() is False
+    if VDBG:
+        print("\nvisual debugging")
+        while not dq.empty():
+            cv2.imshow("pytest", dq.get())
+            cv2.waitKey(20)
+        cv2.destroyAllWindows()
+    else:
+        while not dq.empty():
+            assert dq.get().shape == (128, 256, 3)
+
+
+def test_ti23_welcome_screen():
+    dq = Queue()
+    device = Mocker(dbg_queue=dq)
+    device.capabilities(width=128, height=64, rotate=0, mode="1")
+    start = time()
+    while True:
+        with canvas(device) as draw:
+            welcome_screen(draw)
+
+        sleep(0.02)
+        if time() - start > 3:
+            break
+    device.clear()
+
+    assert dq.empty() is False
+    if VDBG:
+        print("\nvisual debugging")
+        while not dq.empty():
+            cv2.imshow("pytest", dq.get())
+            cv2.waitKey(20)
+        cv2.destroyAllWindows()
+    else:
+        while not dq.empty():
+            assert dq.get().shape == (128, 256, 3)
+
+
+def test_ti23_confirm():
+    dq = Queue()
+    device = Mocker(dbg_queue=dq)
+    device.capabilities(width=128, height=64, rotate=0, mode="1")
+    start = time()
+    while True:
+        with canvas(device) as draw:
+            if time() - start < 1.5:
+                basic_confirm(draw)
+            else:
+                advanced_confirm(draw)
+
+        sleep(0.02)
+        if time() - start > 3:
+            break
+    device.clear()
+
+    assert dq.empty() is False
+    if VDBG:
+        print("\nvisual debugging")
+        while not dq.empty():
+            cv2.imshow("pytest", dq.get())
+            cv2.waitKey(20)
+        cv2.destroyAllWindows()
+    else:
+        while not dq.empty():
+            assert dq.get().shape == (128, 256, 3)
+
+
+def test_ti23_basic_layout():
+    dq = Queue()
+    device = Mocker(dbg_queue=dq)
+    device.capabilities(width=128, height=64, rotate=0, mode="1")
+    start = time()
+    while True:
+        with canvas(device) as draw:
+            basic_layout(draw, (1000, -500), 2000)
+        sleep(0.02)
+        if time() - start > 3:
+            break
+    device.clear()
+
+    assert dq.empty() is False
+    if VDBG:
+        print("\nvisual debugging")
+        while not dq.empty():
+            cv2.imshow("pytest", dq.get())
+            cv2.waitKey(20)
+        cv2.destroyAllWindows()
+    else:
+        while not dq.empty():
+            assert dq.get().shape == (128, 256, 3)
+
+
+def test_ti23_adv_layout():
+    dq = Queue()
+    device = Mocker(dbg_queue=dq)
+    device.capabilities(width=128, height=64, rotate=0, mode="1")
+    start = time()
+    while True:
+        with canvas(device) as draw:
+            adcanced_layout(draw, (1000, -500), 2000)
         sleep(0.02)
         if time() - start > 3:
             break
